cff896d720cdbcbdc1ed3e0222d0747674fcc904
guilherme-sal <salgl@yahoo.com.br>
2021-07-14 15:43:55 -0300

    Corrected dates

--- apicore/functions.py
+++ apicore/functions.py
@@ -18,20 +18,22 @@ def format_alltweets_db_as_aggregated_info_df(df):
     df['tweets'] = 1
     df_info = df[['username', 'date', 'tweets', 'nlikes', 'nreplies', 'nretweets']]
 
+    df_grouped = df_info.groupby('username').sum()
+    df_grouped = df_grouped.astype(int)
+    df_grouped = df_grouped.reset_index()
+    df_grouped = df_grouped.sort_values('username')
+
     last_tweet_list = []
     first_tweet_list = []
-    for target in df_info['username'].unique():
+    for target in df_grouped['username'].unique():
         df_filtered = df_info.query(f'username == "{target}"')
         last_tweet = df_filtered['date'].max()
         first_tweet = df_filtered['date'].min()
         last_tweet_list.append(last_tweet)
         first_tweet_list.append(first_tweet)
-
-    df_grouped = df_info.groupby('username').sum()
-    df_grouped = df_grouped.astype(int)
     df_grouped['first_tweet'] = first_tweet_list
     df_grouped['last_tweet'] = last_tweet_list
-    df_grouped = df_grouped.reset_index()
+
     return df_grouped
 
 
